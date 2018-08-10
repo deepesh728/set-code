579324a2a27d90a3f0f9bdd58b1bf8eb22a71aa5
deepesh728 <deepeshojhaee@gmail.com>
2018-08-10 07:29:53 -0700

    ad

---
+++ rr.py
@@ -0,0 +1,99 @@
+a = "*"
+b = " "
+c = "*" *4
+d = "*" *7
+print("\n")
+print(a *11)
+# print("\n" + a)
+# print("*" *10)
+for y in d:
+	print("{0:<10} {1:<5}".format(y,y))
+print("*" *11)
+
+print("\n")
+# print("{0:<10} {1:<5}".format(a,c))
+print(a *12)
+print("{0:<10} {1:<5}".format(a,b))
+print("{0:<10} {1:<5}".format(a,b))
+print("{0:<10} {1:<5}".format(a,b))
+print(a *12)
+print("{0:<10} {1:<5}".format(a,b))
+print("{0:<10} {1:<5}".format(a,b))
+print("{0:<10} {1:<5}".format(a,b))
+print(a *12)
+print("\n")
+print(a *12)
+print("{0:<10} {1:<5}".format(a,b))
+print("{0:<10} {1:<5}".format(a,b))
+print("{0:<10} {1:<5}".format(a,b))
+print(a *12)
+print("{0:<10} {1:<5}".format(a,b))
+print("{0:<10} {1:<5}".format(a,b))
+print("{0:<10} {1:<5}".format(a,b))
+print(a *12)
+print("\n")
+print(a *11)
+#
+
+print("{0:<10} {1:<5}".format(a,a))
+print("{0:<10} {1:<5}".format(a,a))
+print("{0:<10} {1:<5}".format(a,a))
+print("{0:<10} {1:<5}".format(a,a))
+print(a *11)
+print("{0:<10} {1:<5}".format(a,b))
+print("{0:<10} {1:<5}".format(a,b))
+print("{0:<10} {1:<5}".format(a,b))
+print("{0:<10} {1:<5}".format(a,b))
+
+print("\n")
+print(a *12)
+print("{0:<10} {1:<5}".format(a,b))
+print("{0:<10} {1:<5}".format(a,b))
+print("{0:<10} {1:<5}".format(a,b))
+print(a *12)
+print("{0:<10} {1:<5}".format(a,b))
+print("{0:<10} {1:<5}".format(a,b))
+print("{0:<10} {1:<5}".format(a,b))
+print(a *12)
+print("\n")
+print(a *12)
+for x in c:
+	print(x)
+print(a *11)
+print("{0:<10} {1:<5}".format(b,a))
+print("{0:<10} {1:<5}".format(b,a))
+print("{0:<10} {1:<5}".format(b,a))
+print("{0:<10} {1:<5}".format(b,a))
+print(a *11)
+print("\n")
+
+print("{0:<10} {1:<5}".format(a,a))
+print("{0:<10} {1:<5}".format(a,a))
+print("{0:<10} {1:<5}".format(a,a))
+print(a *12)
+print("{0:<10} {1:<5}".format(a,a))
+print("{0:<10} {1:<5}".format(a,a))
+print("{0:<10} {1:<5}".format(a,a))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ sets.py
@@ -0,0 +1,9 @@
+basket = ['apple', 'orange', 'apple', 'pear', 'orange', 'banana']
+fruit = set(basket)
+print(fruit)
+set(['orange', 'pear', 'apple', 'banana'])
+'orange' in fruit
+True
+'crabgrass' in fruit
+False
+print("my name")
